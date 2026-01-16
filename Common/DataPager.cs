25df01aa393fc11a5066adad53826a0fa2861b81
omkark55 <118451366+omkark55@users.noreply.github.com>
2026-01-16 20:20:36 +0530

    Refactor DataPager by cleaning up comments a
    Removed commented-out code and added a property for Skip calculation.

--- Common/DataPager.cs
+++ Common/DataPager.cs
@@ -5,30 +5,11 @@
         public long RecordCount { get; set; }
         public int PageNumber { get; set; }
         public bool IsFirstHit { get; set; }
-        //public int PageSize { get; private set; }
         public int PageSize { get; set; }
-
         public string SortBy { get; set; }
         public bool IsAscending { get; set; }
-
-        //public int skip { get; set; }
-        public int Skip => (PageNumber > 0 ? PageNumber - 1 : 0) * PageSize;    ///newly added 
-
-        //public static DataPager GetPager(DataPager dataPager,string defaultSort)
-        //{
-        //    int skip = Global.PageSize * (dataPager != null ? (dataPager.PageNumber - 1) : 1);
-        //    string sortBy = (dataPager != null && !string.IsNullOrEmpty(dataPager.SortBy)) ? dataPager.SortBy : defaultSort;
-        //    return new DataPager
-        //    {
-        //        IsFirstHit = dataPager.IsFirstHit,
-        //        IsAscending = dataPager.IsAscending,
-        //        PageNumber = dataPager.PageNumber,
-        //        PageSize = Global.PageSize,
-        //        RecordCount = dataPager.RecordCount,
-        //        skip = skip,
-        //        SortBy = sortBy
-        //    };
-        //}
+        public int Skip => (PageNumber > 0 ? PageNumber - 1 : 0) * PageSize;    
+        
         public string? SearchField { get; set; }
         public string? SearchValue { get; set; }
         public string? SortColumn { get; set; }
