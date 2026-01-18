454ed0bc189441542f5ffd502aca332c1363399a
omkark55 <118451366+omkark55@users.noreply.github.com>
2026-01-18 23:06:56 +0530

    Change Update and Get methods to async
    Updated methods to return Task for asynchronous operations.

--- Common/IBaseRepository.cs
+++ Common/IBaseRepository.cs
@@ -16,13 +16,11 @@ namespace WebApi_Angular.Common
             bool IsTransactionScope { get; set; }
             TDataEntity DTO(object entity);
            
-            ResponseResult Update(TDataEntity entity);
+             Task<ResponseResult> Update(TDataEntity entity);
             
             ResponseResult Delete(TDataEntity entity);
            
-            TDataEntity Get(dynamic parameters, TDataEntity entity);
-
-            IEnumerable<TDataEntity> GetAll(Expression<Func<TDataEntity, bool>> predicate = null);
+             Task<TDataEntity> Get(dynamic parameters, TDataEntity entity);
 
             bool SoftDelete<T>(T id);
             int Commit();
